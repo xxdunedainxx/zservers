dbb442551870c7a0c955d9d931050bdbd9b314d8
Zach McFadden <zach.mcfadden@workday.com>
2020-07-21 20:53:07 -0700

    ?

--- containers/minio/steps/000.install.sh
+++ containers/minio/steps/000.install.sh
@@ -4,3 +4,6 @@ apt-get update --assume-yes
 apt-get upgrade --assume-yes
 
 apt-get install wget --assume-yes
+
+# Needed for WD Passport backend
+sudo apt-get install exfat-fuse exfat-utils --assume-yes
