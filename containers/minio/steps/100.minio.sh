23eecee416b68464d01e65d1dc64304fdcd7ba79
Zach McFadden <zach.mcfadden@workday.com>
2021-02-14 22:39:17 -0800

    local

--- containers/minio/steps/100.minio.sh
+++ containers/minio/steps/100.minio.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 # Download minio 
-wget "https://dl.min.io/server/minio/release/linux-${ARCHITECTURE}/minio"
-
+wget "https://dl.min.io/server/minio/release/${ARCHITECTURE}/minio"
 # Alow execution
 chmod +x minio
 
--- docker_runner.sh
+++ docker_runner.sh
@@ -2,11 +2,16 @@
 # This expects:
 # DISCORD_API_KEY
 source ~/.zservers
-
+# https://stackoverflow.com/questions/13322485/how-to-get-the-primary-ip-address-of-the-local-machine-on-linux-and-os-x
+ip=$(ipconfig getifaddr en0)
 home=$(pwd)
 
 # Build docker images
 
+# Minio
+cd ./containers/minio
+docker build . --build-arg MINIO_PORT=8888 --build-arg ARCHITECTURE=linux-amd64 -t minio
+cd $home
 # VAULT
 
 cd ./containers/vault
@@ -18,6 +23,16 @@ docker build . --build-arg \
        }]}" \
        -t vault
 
+cd $home
+
+cd ./containers/nginx
+docker build . --build-arg NGINX_CONF_OVERRIDE=./configs/nginx-compose --build-arg VAULT_SERVER=${ip}:8200 --build-arg MINIO_SERVER=${ip}:8080 -t ingress
+
 # Generate vault root token
 
 # Run docker compose
+cd $home
+
+cd ./containers
+
+docker-compose up
