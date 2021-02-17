345d5455a0e19ab3fc75f0cf504c46fb49b97267
Zach McFadden <zach.mcfadden@workday.com>
2021-02-16 16:41:27 -0800

    elastic + kibana

--- containers/minio/steps/100.minio.sh
+++ containers/minio/steps/100.minio.sh
@@ -2,11 +2,17 @@
 
 # Download minio 
 wget "https://dl.min.io/server/minio/release/${ARCHITECTURE}/minio"
+
+# download mc client
+wget "https://dl.min.io/client/mc/release/${ARCHITECTURE}/mc"
+
 # Alow execution
 chmod +x minio
+chmod +x mc
 
 # Move to bin for easy execution 
 mv minio /usr/local/bin
+mv mc /usr/local/bin
 
 mkdir -p "${MINIO_SYM_LINK}"
 
--- docker_runner.sh
+++ docker_runner.sh
@@ -6,32 +6,46 @@ source ~/.zservers
 ip=$(ipconfig getifaddr en0)
 home=$(pwd)
 
-# Build docker images
+build_container_service() {
+  # $1 == service to build 
+  # $2 == docker command
+  cd $home
+  cd "./containers/${1}"
+  docker kill $1 || true
+  docker rm $1 || true
+  eval $2
+  cd $home
+}
 
-# Minio
-cd ./containers/minio
-docker build . --build-arg MINIO_PORT=8888 --build-arg ARCHITECTURE=linux-amd64 -t minio
-cd $home
-# VAULT
-
-cd ./containers/vault
-
-docker build . --build-arg \
-       SECRET_JSON_STR="{\"discord\":
-       [{\"key\":\"token\",
-       \"value\":\"${DISCORD_API_KEY}\"
-       }]}" \
-       -t vault
-
-cd $home
-
-cd ./containers/nginx
-docker build . --build-arg NGINX_CONF_OVERRIDE=./configs/nginx-compose --build-arg VAULT_SERVER=${ip}:8200 --build-arg MINIO_SERVER=${ip}:8080 -t ingress
+docker_compose_start() {
+  echo "test"
+}
 
 # Generate vault root token
 
-# Run docker compose
-cd $home
+# Build docker images
+
+# Minio
+build_container_service "minio" "docker build . --build-arg MINIO_PORT=8888 --build-arg ARCHITECTURE=linux-amd64 -t minio"
+
+
+VAULT
+build_container_service "vault" " \
+    docker build . --build-arg \
+       SECRET_JSON_STR=\"{\"discord\":
+       [{\"key\":\"token\",
+       \"value\":\"${DISCORD_API_KEY}\"
+       }]}\" \
+       -t vault"
+
+build_container_service "nginx" " \
+docker build . --build-arg NGINX_CONF_OVERRIDE=./configs/nginx-compose \
+--build-arg VAULT_SERVER=${ip}:8200 --build-arg MINIO_SERVER=${ip}:8080 -t ingress \
+"
+
+build_container_service "elasticsearch" "docker build . -t es"
+
+build_container_service "kibana" "docker build . -t kib"
 
 cd ./containers
 
